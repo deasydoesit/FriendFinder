4b915970a3e7fda8598c049e842ec6c9acb8ab43
deasydoesit <darwin.rinderer@gmail.com>
2018-06-09 22:48:54 -0400

    Creating matching algorithm

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [{name: "George Washington",
+                image: "https://pbs.twimg.com/profile_images/663213215715725314/XGxclZLq_400x400.jpg",
+                scores: [5, 3, 4, 2, 4, 2, 5, 5, 5, 3],
+                total: 38},
+                {name: "Benjamin Franklin",
+                image: "https://pbs.twimg.com/profile_images/594563185023287296/c_Y17CKv_400x400.png",
+                scores: [5, 3, 5, 1, 1, 4, 5, 2, 3, 4],
+                total: 33},
+                {name: "Abraham Lincoln",
+                image: "https://pbs.twimg.com/profile_images/715921622415314944/9o1TdHs7_400x400.jpg",
+                scores: [5, 2, 5, 1, 3, 1, 5, 4, 1, 1],
+                total: 28}];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,40 @@
-var friendsData = require("../data/friends.js");
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
 
-  app.post("/api/friendFinder", function(req, res) {
-    console.log(req.body);
-    res.json(true);
-  });
+    app.post("/api/friendFinder", function(req, res) {
+
+        var total = req.body.scores.reduce(function(accumulator, current) {
+            return accumulator + parseInt(current);
+        }, 0);
+
+        console.log("The total score for the survey is " + total)
+
+        var holder;
+        var index;
+        for (var i = 0; i < friends.length; i++) {
+            var difference = Math.abs(total - friends[i].total);
+            if (i === 0) {
+                holder = difference;
+                index = i;
+            } else if (difference < holder) {
+                holder = difference;
+                index = i;
+            }
+        }
+
+        console.log("The difference between the total score for the survey and the next cloesest is " + holder);
+        console.log("The index of the closest match is " + index);
+        console.log(friends[index]);
+
+        res.json(friends[index]);
+        friends.push(req.body);
+
+        console.log(friends);
+    });
   
 };
